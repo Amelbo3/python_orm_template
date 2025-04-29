59960ab2d7821eef050c8c835ce2111da660a58c
Amelbo3 <amelbo3@gmail.com>
2025-04-29 03:55:36 -0400

    added template for starting python projects
    Signed-off-by: Amelbo3 <amelbo3@gmail.com>

--- models.py
+++ models.py
@@ -3,3 +3,12 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
+# create class model
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
